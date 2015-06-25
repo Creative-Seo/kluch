bb279603d82eaf3a17139833690f652df2f028ca
Alex Illar <cgde88@gmail.com>
2015-06-25 17:31:10 +0300

    besedki-iz-brusa
    новые фоты и данные

--- besedki/besedka-iz-brusa/index.php
+++ besedki/besedka-iz-brusa/index.php
@@ -1,74 +1,32 @@
-<?php $meta_container = '<link rel="stylesheet" href="http://www.kluch585.ru/css/jquery.fancybox.css"><link rel="stylesheet" href="http://www.kluch585.ru/js/helpers/jquery.fancybox-thumbs.css">'; ?>
-<?php $footer_block = '<script src="http://www.kluch585.ru/js/jquery.fancybox.js"></script><script src="http://www.kluch585.ru/js/jquery.mousewheel-3.0.6.pack.js"></script><script src="http://www.kluch585.ru/js/helpers/jquery.fancybox-thumbs.js"></script> 
-<script type="text/javascript">
-$(document).ready(function() {
-	$(".fancybox-thumb").fancybox({
-		prevEffect	: \'none\',
-		nextEffect	: \'none\',
-		showNavArrows : \'true\',
-		helpers	: {
-			title	: {
-				type: \'outside\'
-			},
-			thumbs	: {
-				width	: 50,
-				height	: 50
-			}
-		}
-	});
-});
-</script>
-<script type="text/javascript">
-$(document).ready(function() {
-	$(".fancybox-thumb2").fancybox({
-		prevEffect	: \'none\',
-		nextEffect	: \'none\',
-		showNavArrows : \'true\',
-		helpers	: {
-			title	: {
-				type: \'outside\'
-			},
-			thumbs	: {
-				width	: 50,
-				height	: 50
-			}
-		}
-	});
-});
-</script>
-'; ?>
-<?php $title = 'Беседка Гармония от производителя по доступной цене во Владимире'; ?>
+<?php $title = 'Беседка из бруса от производителя по доступной цене во Владимире'; ?>
 <?php $description = ''; ?>
 <?php $keywords = ''; ?>
 <?php $breadcrumb2 = 'Беседки'; ?>
 <?php $breadcrumb2_url = 'http://www.kluch585.ru/besedki/'; ?>
-<?php $breadcrumb = 'Беседка Гармония'; ?>
+<?php $breadcrumb = 'Беседка из бруса'; ?>
 <?php $canonical = 'http://www.kluch585.ru/besedki/garmoniya/'; ?>
 <?php include("../../blocks/meta.php"); ?>
 <?php include("../../blocks/header.php"); ?>
 <?php include("../../blocks/breadcrumbs.php"); ?>
 <div class="container proekt-style">
   <div class="row">
-    <h1>Беседка Гармония</h1>
+    <h1>Беседка из бруса</h1>
     <div class="col-lg-1 col-md-1 hidden-sm hidden-xs" style="padding:0"> 
        </div>
     <div class="col-lg-8 col-md-8 col-sm-6 col-xs-12"> 
-    <a class="fancybox-thumb" rel="gallery1" href="http://www.kluch585.ru/besedki/garmoniya/besedka-garmoniya.jpg"><img src="http://www.kluch585.ru/besedki/garmoniya/garmoniya.jpg" class="img-responsive" alt="Беседка Гармония"/></a>
+    <a class="fancybox-thumb" rel="gallery1" href="http://www.kluch585.ru/besedki/besedka-iz-brusa/besedka-iz-brusa.jpg">
+		<img src="http://www.kluch585.ru/besedki/besedka-iz-brusa/besedka-iz-brusa.jpg" class="img-responsive" alt="Беседка из бруса"/></a>
     <!--gallery-->  
 
    </div>
-    
-    
 
     <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12">
       <div class="row">
        
-        <div class="cena-block"> <span class="price-proekt2">Стоимость строительства:</span> <span class="price-proekt3">35000 </span>руб.<br>
+        <div class="cena-block"> <span class="price-proekt2">Стоимость строительства:</span> <span class="price-proekt3">договорная </span><br>
           <button type="submit" class="button-style open-modal">Оставить заявку</button>
      <?php include("../../blocks/modal-zakaz.php"); ?>     
-     
-            
-            
+
           </div>
 		  
           <div class="modal-footer modal-otpr">
@@ -86,7 +44,7 @@ $(document).ready(function() {
               <tbody>
                 <tr>
                   <th scope="row">Размер беседки:</th>
-                  <td>2,75x2,75 м</td>
+                  <td></td>
                 </tr>
                 <tr>
                   <th scope="row">Срок постройки:</th>
@@ -109,22 +67,27 @@ $(document).ready(function() {
    <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">  
      <div class="opisanie table-responsive">
      <div><h2>Описание</h2></div>
-        <p>В стоимость входит:  Беседка с тремя стационарными лавками <p>
-        <p>Можно доп. заказать: монтаж беседки, устройство основания, окраска, стол, лавочки.</p>
+		<p>Мы строим беседки из бруса под ключ : от фундамента до кровли. Для расчёта стоимости нам необходимо знать размер беседки, тип фундамента, тип кровли, место установки.</p>
+		<p>Выбор проектов беседок из бруса очень велик, предлагаем несколько вариантов на наше усмотрение, если же у Вас есть свой проект, присылайте, посчитаем.</p>
         </div>
      </div>
      
      <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12 hidden-xs">
-        <h2>Беседки построенные по проекту</h2>
+        <h2>Примеры беседок из бруса</h2>
         
          <div class="gallery2">
-    <a class="fancybox-thumb2" rel="gallery1" href="http://www.kluch585.ru/besedki/garmoniya/besedka-garmoniya.jpg"><img src="http://www.kluch585.ru/besedki/garmoniya/besedka-garmoniya.jpg" width="150" class="img-responsive" alt="<?php echo $title; ?>"/></a>
-    <a class="fancybox-thumb2" rel="gallery1" href="http://www.kluch585.ru/besedki/garmoniya/besedka-garmoniya-1.jpg"><img src="http://www.kluch585.ru/besedki/garmoniya/besedka-garmoniya-1.jpg" width="150" class="img-responsive" alt="<?php echo $title; ?>"/></a> 
-    <a class="fancybox-thumb2" rel="gallery1" href="http://www.kluch585.ru/besedki/garmoniya/besedka-garmoniya-1.jpg"><img src="http://www.kluch585.ru/besedki/garmoniya/besedka-garmoniya-2.jpg" width="150" class="img-responsive" alt="<?php echo $title; ?>"/></a> 
-    <a class="fancybox-thumb2" rel="gallery1" href="http://www.kluch585.ru/besedki/garmoniya/besedka-garmoniya-2.jpg"><img src="http://www.kluch585.ru/besedki/garmoniya/besedka-garmoniya-3.jpg" width="150" class="img-responsive" alt="<?php echo $title; ?>"/></a> 
-    <a class="fancybox-thumb2" rel="gallery1" href="http://www.kluch585.ru/besedki/garmoniya/besedka-garmoniya-3.jpg"><img src="http://www.kluch585.ru/besedki/garmoniya/besedka-garmoniya-4.jpg" width="150" class="img-responsive" alt="<?php echo $title; ?>"/></a> 
-    <a class="fancybox-thumb2" rel="gallery1" href="http://www.kluch585.ru/besedki/garmoniya/besedka-garmoniya-4.jpg"><img src="http://www.kluch585.ru/besedki/garmoniya/besedka-garmoniya-5.jpg" width="150" class="img-responsive" alt="<?php echo $title; ?>"/></a> 
-    <a class="fancybox-thumb2" rel="gallery1" href="http://www.kluch585.ru/besedki/garmoniya/besedka-garmoniya-5.jpg"><img src="http://www.kluch585.ru/besedki/garmoniya/besedka-garmoniya-6.jpg" width="150" class="img-responsive" alt="<?php echo $title; ?>"/></a> 
+<?php for ($i=0;i<17;$i++) {?>
+    <a class="fancybox-thumb2" rel="gallery1" href="http://www.kluch585.ru/besedki/besedka-iz-brusa/besedka-iz-brusa<?php echo $i;?>.jpg" title="Беседка из бруса <?php echo $i;?>">
+	<img src="http://www.kluch585.ru/besedki/besedka-iz-brusa/besedka-iz-brusa<?php echo $i;?>.jpg" width="150" class="img-responsive" alt="<?php echo $title; ?>"/></a>
+<?php } ?>
+    <a class="fancybox-thumb2" rel="gallery1" href="http://www.kluch585.ru/besedki/besedka-iz-brusa/besedka-dlya-sada.jpg" title="Беседка для сада">
+		<img src="http://www.kluch585.ru/besedki/besedka-iz-brusa/besedka-dlya-sada.jpg" width="150" class="img-responsive" alt="<?php echo $title; ?>"/></a>
+    <a class="fancybox-thumb2" rel="gallery1" href="http://www.kluch585.ru/besedki/besedka-iz-brusa/besedka-gril.jpg" title="Беседка гриль">
+		<img src="http://www.kluch585.ru/besedki/besedka-iz-brusa/besedka-gril.jpg" width="150" class="img-responsive" alt="<?php echo $title; ?>"/></a>
+    <a class="fancybox-thumb2" rel="gallery1" href="http://www.kluch585.ru/besedki/besedka-iz-brusa/besedka-romantika.jpg" title="Беседка Романтика">
+		<img src="http://www.kluch585.ru/besedki/besedka-iz-brusa/besedka-romantika.jpg" width="150" class="img-responsive" alt="<?php echo $title; ?>"/></a>
+    <a class="fancybox-thumb2" rel="gallery1" href="http://www.kluch585.ru/besedki/besedka-iz-brusa/besedka-s-kuhney.jpg" title="Беседка с кухней">
+		<img src="http://www.kluch585.ru/besedki/besedka-iz-brusa/besedka-s-kuhney.jpg" width="150" class="img-responsive" alt="<?php echo $title; ?>"/></a>
        </div>
         
         
