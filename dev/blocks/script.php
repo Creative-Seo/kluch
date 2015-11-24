79cd66221a23f73d1e23271cf49eeeb0da54b105
ilcomp <cgde88@gmail.com>
2015-11-24 14:25:15 +0300

    u

--- dev/blocks/script.php
+++ dev/blocks/script.php
@@ -2,10 +2,9 @@
 <script>
 (function (d, w, c) { (w[c] = w[c] || []).push(function() { try { w.yaCounter21486487 = new Ya.Metrika({id:21486487, webvisor:true, clickmap:true, trackLinks:true, accurateTrackBounce:true}); } catch(e) { } }); var n = d.getElementsByTagName("script")[0], s = d.createElement("script"), f = function () { n.parentNode.insertBefore(s, n); }; s.type = "text/javascript"; s.async = true; s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js"; if (w.opera == "[object Opera]") { d.addEventListener("DOMContentLoaded", f, false); } else { f(); } })(document, window, "yandex_metrika_callbacks");
 </script>
-
 <script>
-var ms=document.createElement("link");ms.rel="stylesheet";
-/*ms.href="http://fonts.googleapis.com/css?family=Open+Sans:300&subset=latin,cyrillic";document.getElementsByTagName("head")[0].appendChild(ms);
+/*var ms=document.createElement("link");ms.rel="stylesheet";
+ms.href="http://fonts.googleapis.com/css?family=Open+Sans:300&subset=latin,cyrillic";document.getElementsByTagName("head")[0].appendChild(ms);
 var ms=document.createElement("link");ms.rel="stylesheet";
 ms.href="/css/bootstrap.min.css";document.getElementsByTagName("head")[0].appendChild(ms);
 var ms=document.createElement("link");ms.rel="stylesheet";
@@ -18,20 +17,32 @@ var ms=document.createElement("link");ms.rel="stylesheet";
 ms.href="/css/style.css";document.getElementsByTagName("head")[0].appendChild(ms);
 </script>
 
+
+<!-- build:remove -->
 <script>
-var scr = {"scripts":[{"src" : "/js/jquery-2.1.1.min.js", "async" : false},
+var scr = {"scripts":[
+{"src" : "/libs/jquery/dist/jquery.min.js", "async" : false},
+{"src" : "/libs/bootstrap-sass/assets/javascripts/bootstrap.min.js", "async" : false},
 {"src" : "/js/jquery.mobile.custom.min.js", "async" : false},
-{"src" : "/js/jquery.fancybox.pack.js", "async" : false},
-{"src" : "/js/jquery.fancybox-thumbs.js", "async" : false},
+{"src" : "/libs/fancybox/source/jquery.fancybox.pack.js", "async" : false},
+{"src" : "/libs/fancybox/source/helpers/jquery.fancybox-thumbs.js", "async" : false},
 {"src" : "/js/blocksit.min.js", "async" : false},
 {"src" : "/js/main.js", "async" : false},
-{"src" : "/js/bootstrap.min.js", "async" : false},
 {"src" : "/js/callback.js", "async" : false},
 {"src" : "/js/jquery.maskedinput.min.js", "async" : false},
 {"src" : "/js/jquery.stickyheader.js", "async" : false},
 {"src" : "/js/jquery.ba-throttle-debounce.min.js", "async" : false},
 {"src" : "/js/common.js", "async" : false},]};
-!function(t,n,r){"use strict";var c=function(t){if("[object Array]"!==Object.prototype.toString.call(t))return!1;for(var r=0;r<t.length;r++){var c=n.createElement("script"),e=t[r];c.src=e.src,c.async=e.async,n.body.appendChild(c)}return!0};t.addEventListener?t.addEventListener("load",function(){c(r.scripts);},!1):t.attachEvent?t.attachEvent("onload",function(){c(r.scripts)}):t.onload=function(){c(r.scripts)}}(window,document,scr);</script>
+!function(t,n,r){"use strict";var c=function(t){if("[object Array]"!==Object.prototype.toString.call(t))return!1;for(var r=0;r<t.length;r++){var c=n.createElement("script"),e=t[r];c.src=e.src,c.async=e.async,n.body.appendChild(c)}return!0};t.addEventListener?t.addEventListener("load",function(){c(r.scripts);},!1):t.attachEvent?t.attachEvent("onload",function(){c(r.scripts)}):t.onload=function(){c(r.scripts)}}(window,document,scr);
+</script>
+<!-- /build -->
+
+<!-- build:template
+<script>
+var scr = {"scripts":[{"src" : "/js/scripts.min.js", "async" : false}]};
+!function(t,n,r){"use strict";var c=function(t){if("[object Array]"!==Object.prototype.toString.call(t))return!1;for(var r=0;r<t.length;r++){var c=n.createElement("script"),e=t[r];c.src=e.src,c.async=e.async,n.body.appendChild(c)}return!0};t.addEventListener?t.addEventListener("load",function(){c(r.scripts);},!1):t.attachEvent?t.attachEvent("onload",function(){c(r.scripts)}):t.onload=function(){c(r.scripts)}}(window,document,scr);
+</script>
+/build -->
 
  <script>   
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
@@ -42,7 +53,6 @@ ga('create', 'UA-58917038-1', 'auto');ga('send', 'pageview');
 (function(){ var widget_id = 'cigYeBitsb';var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);})();
 </script>
 <!-- {/literal} END JIVOSITE CODE -->
-<!-- Include all compiled plugins (below), or include individual files as needed -->
 
 <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
--- dev/foto-otchyot/content.php
+++ dev/foto-otchyot/content.php
@@ -6,63 +6,23 @@ include($root."blocks/breadcrumbs.php");?>
 <div class="container">
   <div class="row">
     <h1>Фото-отчёт</h1>
-    <div class="gallery foto-otchyot">
-    <div class="col-md-3">
-    <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/1.jpg"><img src="/foto-otchyot/1.jpg" class="img-responsive" alt=""/></a>
-    </div>
-    <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/2.jpg"><img src="/foto-otchyot/2.jpg" class="img-responsive" alt=""/></a>
-     </div>
-     <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/3.jpg"><img src="/foto-otchyot/3.jpg" class="img-responsive" alt=""/></a>
-     </div>
-     <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/4.jpg"><img src="/foto-otchyot/4.jpg" class="img-responsive" alt=""/></a>
-     </div>
-     <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/5.jpg"><img src="/foto-otchyot/5.jpg" class="img-responsive" alt=""/></a>
-     </div>
-     <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/6.jpg"><img src="/foto-otchyot/6.jpg" class="img-responsive" alt=""/></a>
-     </div>
-     <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/7.jpg"><img src="/foto-otchyot/7.jpg" class="img-responsive" alt=""/></a>
-     </div>
-     <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/8.jpg"><img src="/foto-otchyot/8.jpg" class="img-responsive" alt=""/></a>
-     </div>
-     <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/9.jpg"><img src="/foto-otchyot/9.jpg" class="img-responsive" alt=""/></a>
-     </div>
-     <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/10.jpg"><img src="/foto-otchyot/10.jpg" class="img-responsive" alt=""/></a>
-     </div>
-     <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/11.jpg"><img src="/foto-otchyot/1.jpg" class="img-responsive" alt=""/></a>
-     </div>
-     <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/12.jpg"><img src="/foto-otchyot/12.jpg" class="img-responsive" width="263" alt=""/></a>
-     </div>
-
-     <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/14.jpg"><img src="/foto-otchyot/14.jpg" width="263" class="img-responsive" alt=""/></a>
-     </div>
-
-     <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/16.jpg"><img src="/foto-otchyot/16.jpg" class="img-responsive" alt=""/></a>
-     </div>
-     <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/17.jpg"><img src="/foto-otchyot/17.jpg" class="img-responsive" alt=""/></a>
-     </div>
-     <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/18.jpg"><img src="/foto-otchyot/18.jpg" class="img-responsive" alt=""/></a>
-     </div>
-          <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/15.jpg"><img src="/foto-otchyot/15.jpg" class="img-responsive" alt=""/></a>
-     </div>
-          <div class="col-md-3">
-     <a class="fancybox-thumb" rel="gallery1" href="/foto-otchyot/13.jpg"><img src="/foto-otchyot/13.jpg" class="img-responsive" alt="" width="263"></a>
-     </div>
+    <div id="container" class="gallery foto-otchyot">
+    
+<?php 
+$images = scandir('./');
+if (false !== $images) {
+    $imgarray = preg_grep('/\\.(?:jpe?g)$/', $images);
+    foreach($imgarray as $row) { 
+?>  
+        <div class="grid">
+        <div class="imgholder image-thumb">
+            <a class="fancybox-thumb" rel="gallery1" href="<?php echo htmlspecialchars(urlencode($row));?>">
+            <figure class=" animated zoomIn">
+            <img src="<?php echo htmlspecialchars(urlencode($row));?>" class="img-responsive" alt="Дачный дом - Ключ 585"/>
+            </figure></a>
+        </div></div>
+<?php }} ?>
+ 
     </div>
   </div>
 </div>    
